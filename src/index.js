96a5ac8161e376931aa685451b712e524621a6c9
DAVINA <davinabuansi@gmail.com>
2024-11-25 14:37:19 +0000

    pushing all changes

---
+++ src/index.js
@@ -0,0 +1,5 @@
+let name= prompt("what is your name ?");
+let age= prompt("how old are you?");
+let email= prompt("type your email");
+
+alert(`Hi ${name} you are ${age} years old and we will keep in touch via your email:${email} `)
